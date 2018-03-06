afd128b08b46900394d3976ebfb67ddef42edffe
craigray <craig@craigrayner.com>
2018-03-06 15:27:40 +1100

    Added DateTimeType for wrapTime
    Signed-off-by: craigray <craig@craigrayner.com>

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Symfony 4 Form Extension Bundle
 
 Version
 -------
-0.1.01
+0.1.02
 
 Development ONLY
 ----------------
--- Util/VersionManager.php
+++ Util/VersionManager.php
@@ -3,7 +3,7 @@ namespace Hillrange\Form\Util;
 
 class VersionManager
 {
-    const VERSION = '0.1.01';
+    const VERSION = '0.1.02';
 
     /**
      * @return string
