37abdb6754f62f68c13deac7fe66d0ad991127a0
craigray <craig@craigrayner.com>
2018-03-27 14:54:10 +1100

    Version Only
    Signed-off-by: craigray <craig@craigrayner.com>

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Symfony 4 Form Extension Bundle
 
 Version
 -------
-0.1.28
+0.1.29
 
 Development ONLY
 ----------------
--- Util/VersionManager.php
+++ Util/VersionManager.php
@@ -3,7 +3,7 @@ namespace Hillrange\Form\Util;
 
 class VersionManager
 {
-    const VERSION = '0.1.28';
+    const VERSION = '0.1.29';
 
     /**
      * @return string
