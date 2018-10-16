754f9cd4a1730611ead85987a337a022ca84dd65
Craig Rayner <craig@craigrayner.com>
2018-10-16 11:51:44 +1100

    Change help to default of false not null

--- Type/Extension/FieldHelpTypeExtension.php
+++ Type/Extension/FieldHelpTypeExtension.php
@@ -15,6 +15,13 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class FieldHelpTypeExtension extends AbstractTypeExtension
 {
+    /**
+     * buildView
+     *
+     * @param FormView $view
+     * @param FormInterface $form
+     * @param array $options
+     */
 	public function buildView(FormView $view, FormInterface $form, array $options)
 	{
 		$view->vars['help'] = $options['help'];
@@ -30,11 +37,16 @@ class FieldHelpTypeExtension extends AbstractTypeExtension
         $view->vars['attr'] = $attr;
 	}
 
+    /**
+     * configureOptions
+     *
+     * @param OptionsResolver $resolver
+     */
 	public function configureOptions(OptionsResolver $resolver)
 	{
 		$resolver->setDefaults(
 			[
-				'help'  => null,
+				'help'  => false,
 				'help_params' => [],
                 'element_class' => '',
                 'auto_complete' => 'off',
@@ -42,6 +54,11 @@ class FieldHelpTypeExtension extends AbstractTypeExtension
 		);
 	}
 
+    /**
+     * getExtendedType
+     *
+     * @return string
+     */
 	public function getExtendedType()
 	{
 		return FormType::class;
