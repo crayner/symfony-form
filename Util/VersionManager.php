d1de2f7f2acbe14fed93827f439aa9b5cf6d22d7
Craig Rayner <craig@craigrayner.com>
2018-11-15 15:07:21 +1100

    AlwaysInValidValidator Tweak

--- Util/VersionManager.php
+++ Util/VersionManager.php
@@ -10,7 +10,7 @@ class VersionManager
     /**
      * String
      */
-    const VERSION = '0.3.21';
+    const VERSION = '0.3.22';
 
     /**
      * getVersion
--- Validator/Constraints/AlwaysInValidValidator.php
+++ Validator/Constraints/AlwaysInValidValidator.php
@@ -18,8 +18,21 @@ class AlwaysInValidValidator extends ConstraintValidator
      */
     public function validate($value, Constraint $constraint)
     {
+        $object = $this->context->getObject();
+        $name = null;
+        if (is_object($object)) {
+            if (method_exists($object, 'getConfig'))
+                $name = $object->getConfig()->getName();
+            if (! $name && method_exists($object, '__toString'))
+                $name = $object->__toString();
+            if (! $name && method_exists($object, 'getName'))
+                $name = $object->getName();
+            if (! $name)
+                $name = get_class($object);
+        } else
+            $name = strval($object);
         $this->context->buildViolation($constraint->message)
-            ->setParameter('%{name}', $this->context->getObject()->getConfig()->getName())
+            ->setParameter('%{name}', $name)
             ->addViolation();
     }
 }
